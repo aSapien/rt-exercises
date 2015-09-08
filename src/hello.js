c8ad12d77b053018556451c9133002aae77e842e
Dima_Ryskin <dimar@wix.com>
2015-09-08 18:44:20 +0300

    Exercise 3: get name from url

--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,28 @@
 define(['react', 'lodash', './hello.rt.js'], function (React, _, template) {
     'use strict';
+    function getEmptyMap() {
+        return Object.create(null);
+    }
+
+    function getQueryParamsMap() {
+        var result = getEmptyMap();
+
+        var queryParamsString = location.search.substr(1);
+        var queryParamsPairs = queryParamsString.split('&');
+        queryParamsPairs.forEach(function(pair) {
+            var splitPair = pair.split('=');
+            result[splitPair[0]] = splitPair[1];
+        });
+
+        return result;
+    }
+
     return React.createClass({
         getInitialState: function () {
+            var queryParams = getQueryParamsMap();
+
             return {
-                name: 'Dima'
+                name: queryParams.name || 'Dima'
             }
         },
         onNameChange: function (event) {
