7a1c7f84c35cd975f5009b7a43bc7487068c1ed2
Dima_Ryskin <dimar@wix.com>
2015-09-08 18:33:45 +0300

    Exercise 2: input form

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,16 @@
 define(['react', 'lodash', './hello.rt.js'], function (React, _, template) {
     'use strict';
     return React.createClass({
+        getInitialState: function () {
+            return {
+                name: 'Dima'
+            }
+        },
+        onNameChange: function (event) {
+            event.preventDefault();
+            this.state.name = event.target.value;
+            this.setState({name: this.state.name});
+        },
         displayName: 'Hello',
         render: template
     });
